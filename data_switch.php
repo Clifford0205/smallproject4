f25db2c4700fa756e039c1b6c3a5db8919ca952e
甜甜 <ts02690140@gmail.com>
2019-04-08 16:03:24 +0800

    04081605

--- data_switch.php
+++ data_switch.php
@@ -1,40 +1,55 @@
 <?php
 
-require __DIR__. '/__connect_db.php';
+require __DIR__ . '/__connect_db.php';
 
 $sid = isset($_GET['sid']) ? intval($_GET['sid']) : 0;
 
-$pdo->query("UPDATE `member` SET `m_active`=NOT m_active WHERE `m_sid` =$sid");
+$sql="SELECT `m_sid`,`m_active` FROM `member` WHERE `m_sid` =$sid ";
+$stmt = $pdo->query($sql);
+$data = $stmt->fetch(PDO::FETCH_ASSOC);
 
-$goto = 'data_list4  .php'; // 預設值
+// echo $data;
+print_r($data['m_active']);
 
-$page=$_GET['page'];
+if($data['m_active']==="正常"){
+    $pdo->query("UPDATE `member` SET `m_active`='停權' WHERE `m_sid` =$sid  && `m_active`='正常'");
+}
 
-$perPage=$_GET['perPage'];
+if($data['m_active']==="停權"){
+    $pdo->query("UPDATE `member` SET `m_active`='正常' WHERE `m_sid` =$sid  && `m_active`='停權'");
+}
 
-$city=$_GET['city'];
+// $pdo->query("UPDATE `member` SET `m_active`='停權' WHERE `m_sid` =$sid  && `m_active`='正常'");
+// $pdo->query("UPDATE `member` SET `m_active`='正常' WHERE `m_sid` =$sid  && `m_active`='停權'");
+
+$goto = 'data_list2.php'; // 預設值
+
+$page = $_GET['page'];
+
+$perPage = $_GET['perPage'];
+
+$city = $_GET['city'];
 
 
-$city=$_GET['city'];
+$city = $_GET['city'];
 
-$keyword =$_GET['keyword'];
+$keyword = $_GET['keyword'];
 
-$sortway=$_GET['sortway'];
+$sortway = $_GET['sortway'];
 
-$temp = explode("#",$_SERVER['HTTP_REFERER'])[0];
+$temp = explode("#", $_SERVER['HTTP_REFERER'])[0];
 
 echo $temp;
 
 echo "<br>";
 
-$url=$temp."#".$page."&perPage=".$perPage."&city=".$city."&sortway=".$sortway."&keyword=".$keyword;
+$url = $temp . "#" . $page . "&perPage=" . $perPage . "&city=" . $city . "&sortway=" . $sortway . "&keyword=" . $keyword;
 
- echo $url;
+echo $url;
 
-if(isset($_SERVER['HTTP_REFERER'])){
-    
+if (isset($_SERVER['HTTP_REFERER'])) {
+
     $goto = $url;
 }
 
 header("Location: $goto");
-
